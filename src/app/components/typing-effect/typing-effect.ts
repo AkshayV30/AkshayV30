9fb3f219b873d28fdfb69dd37cdaac8af7745035
AkshayV30 <akshayatwork.v30@gmail.com>
2025-09-25 01:22:13 +0530

    update back to react

--- src/app/components/typing-effect/typing-effect.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-typing-effect',
-  imports: [],
-  templateUrl: './typing-effect.html',
-  styleUrl: './typing-effect.scss',
-})
-export class TypingEffect {}
